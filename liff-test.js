217cdba09d8c726e7332b13b77be505d7f7d4a24
LeoChien <leochien@243studios.com>
2022-05-21 14:43:03 +0800

    [Test] API testing

--- liff-test.js
+++ liff-test.js
@@ -6,43 +6,27 @@ function shareMessage()
         liffId: '1657152200-LPb0V708',
         withLoginOnExternalBrowser: true,
     }).then(() => {
-        if(!liff.isLoggedIn() || !liff.isInClient()){
+        if(!liff.isLoggedIn()){
             liff.login({redirectUri: 'https://heartkage.github.io/LIFF-APP/'});
+        }
 
-            var warning = "[Login] You are not login!";
-            console.log(warning)
-            window.alert(warning);
+        const accessToken = liff.getAccessToken();
+            
+        if(!liff.isInClient()){
+            window.alert('Please use it in Line APP');
         }
         else{
-            const accessToken = liff.getAccessToken();
-            console.log(accessToken);
-            window.alert(accessToken);
-
-            liff.getFriendship().then((data) =>{
-                if(data.friendFlag)
-                {
-                    if(!liff.isInClient()){
-                        window.alert('Please press it in Line APP');
-                    }
-                    else{
-                        liff.sendMessages([{
-                            type: 'text',
-                            text: 'Hello World',
-                        }]).then(() =>{
-                            window.alert('[Success] Your message has been sent!');
-                        }).catch((error) =>{
-                            window.alert(`[Failed] Error: ${error}`);
-                        });
-                    }
-                }
-                else
-                {
-                    var message = "FriendFlag is false";
-                    console.log(message);
-                    window.alert(message);
-                }
+            liff.sendMessages([{
+                type: 'text',
+                text: 'Hello World',
+            }]).then(() =>{
+                window.alert('[Success] Your message has been sent!');
+            }).catch((error) =>{
+                window.alert(`[Failed] Error: ${error}`);
             });
         }
+
+        //liff.closeWindow();
     });
 }
 
